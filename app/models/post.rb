570d77d0b4cc069fb7d8b2a6206939be271c8d26
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-04-02 19:18:45 -0300

    new association for Model Comment added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
+
   belongs_to :user
+  has_many :comments
 
   validates :title, presence: true, uniqueness: true, length: {minimum:4, maximum:20}
   validates :body, presence: true
