6432ef38d12b5f878a1cee37f3e48cae9ca4f928
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-04-02 15:16:42 -0300

    new User Model created and configured and first user registered

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true, length: { minimum: 4, maximum: 25 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 8 }, format: {
+    with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/,
+    message: "must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number and one symbol"
+  }
+  validates :password_confirmation, presence: true, confirmation: { message: "must match password" }
+end
